d21bb44aeaa5daa5c009bd21d2ddf3656c6a355b
erisalke <erisalke@gmail.com>
2016-12-06 19:11:00 +0100

    input for responds to enter

--- src/components/simple/inputForm.js
+++ src/components/simple/inputForm.js
@@ -1,25 +1,23 @@
 import React from 'react';
 
-
 export default function (props) {
-	var input;
+	let input;
 
   return (
 		<div>
-			<input ref={text => {
-				input = text;
-			}} />
-			{' '}
-			<button
-        type="button"
-        className="btn btn-default"
-        onClick={() => {
-					props.onClick(input.value)
-					input.value = ''
-				}}>
-          { props.buttonName }
-      </button>
+      <input
+				type="text"
+				placeholder={ props.placeholder }
+				ref={text => {
+					input = text;
+				}}
 
+				onKeyPress={(e) => {
+	        if (e.key === 'Enter') {
+						props.onClick(input.value)
+						input.value = ''
+	        }
+				}} />
 		</div>
   );
 };
